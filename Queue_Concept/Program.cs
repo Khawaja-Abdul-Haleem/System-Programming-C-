4a618c9b6af0681fa5c1dfaeed4ca6e8912c257e
Khawaja Abdul Haleem <59179832+Khawaja-Abdul-Haleem@users.noreply.github.com>
2021-04-11 00:35:09 +0500

    Add files via upload

---
+++ Queue_Concept/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Collections;
+using System.Linq;
+using System.Text;
+
+namespace Queue_Concept
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue q = new Queue();
+
+            q.Enqueue('K');
+            q.Enqueue('A');
+            q.Enqueue('H');
+            
+
+            Console.WriteLine("Current queue: ");
+            foreach (char c in q) {
+                Console.Write(c + " ");
+            }
+            
+
+            Console.WriteLine();
+            q.Enqueue('A');
+            q.Enqueue('B');
+            Console.WriteLine("Current queue: ");
+            foreach (char c in q) Console.Write(c + " ");
+
+            Console.WriteLine();
+            Console.WriteLine("Removing some values ");
+            char ch = (char)q.Dequeue();
+
+            Console.WriteLine("The removed value: {0}", ch);
+            ch = (char)q.Dequeue();
+            Console.WriteLine("The removed value: {0}", ch);
+
+            Console.WriteLine("Current queue: ");
+            foreach (char c in q) Console.Write(c + " ");
+
+            Console.ReadKey();
+        }
+    }
+}
